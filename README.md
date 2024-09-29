fc017bd9bf27f5774e2dfb139ecc81c0600fb881
dieter whittingham <dieter.whitt@gmail.com>
2024-09-28 21:40:09 -0400

    filled out readme

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # FizzBuzz Copyable 2
 
-Popular problem FizzBuzz implemented in ARM64 for XNU (MacOS).
+Popular problem FizzBuzz implemented in ARM64 for the XNU kernel (MacOS).
+
+## Compatibility
+
+First ensure that your device architecture is compatible with the program:
+
+```bash
+uname -a
+```
+
+Confirm that your device is using ARM64 on a XNU kernel (darwin).
+
+## Compliation
+
+#### Clang:
+
+```bash
+clang fizzbuzz.s -o fizzbuzz -e _start
+```
+
+#### Assembler
+
+```bash
+as -o fizzbuzz.o fizzbuzz.s
+ld -o fizzbuzz fizzbuzz.o
+```
+
+## Execution
+
+```bash
+./fizzbuzz n
+```
+
+Where n is an integer.
--- fizzbuzz.s
+++ fizzbuzz.s
@@ -22,19 +22,75 @@ sp: .ascii " "
 .align 2
 
 _start:
+    bl _iterate
     b _terminate
 
+// performs fizzbuzz operation with n in x15
+_iterate:
+    loop:
+        mov x14 x15 // fizzbuzz divisor
+        bl modulo // get remainder
+        cmp x13 #0 // check if 15 divides index
+        beq
+
+
+        sub x15 x15 #1 // subtract index
+        cmp x15 #0 
+        beq loop // iterate while index is still positive
+    ret
+
+// modulo operator
+// x15 % x14 -> x13
+_modulo:
+    udiv x12 x15 x14 // quotient stored in x12
+    mul x11 x12 x14 // multiply by smaller number and store in 11
+    sub x13 x15 x11 // x15 - x11 yields remainder
+    ret
+
 // gets command line arg and converts to int
+// loads to x15
 // exits w/ error if invalid
 // exits w/ error if >1 args
 _loadarg:
     ret
 
-// prints string
-_prints:
+// prints fizzbuzz
+_printfizzbuzz:
+    // write: x0-output stream x1-address of string x2-size of string in bytes
+    // system call command: 4
+    mov     x0, #1 // stdout
+    adr     x1, fizzbuzz // string label
+    mov     x2, #8 // 8 bytes long
+    mov     x16, #4 // write command
+    svc     0
+    ret
+
+_printfizz:
+    mov     x0, #1 // stdout
+    adr     x1, fizz
+    mov     x2, #4 
+    mov     x16, #4 
+    svc     0
+    ret
+
+_printbuzz:
+    mov     x0, #1 // stdout
+    adr     x1, buzz
+    mov     x2, #4 
+    mov     x16, #4 
+    svc     0
+    ret
+
+_printsp:
+    mov     x0, #1 // stdout
+    adr     x1, sp
+    mov     x2, #1
+    mov     x16, #4
+    svc     0
     ret
 
 // prints integer
+// integer in x15
 _printi:
     ret
 
